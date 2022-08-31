d85a2c151a5ae4aa69c4521bc541a2a7988fcbe2
Christopher Roe <cpr@tweega.com>
2022-08-31 15:01:52 +0100

    Initial commit

---
+++ some_code.py
@@ -0,0 +1,13 @@
+
+def somethingWithInts(a: int, b: int):
+    j = 8 + a * b
+    k = 9 - a // b
+    
+    j + k
+
+
+def somethingElseWithInts(a: int, b: int):
+    j = a * b // 3
+    k = (9 - a) * b
+    
+    j + k
